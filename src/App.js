e6dce60c94b6f95c2e0760c99a4b3a8036d401e4
Sujeet17m <sujeet17das@gmail.com>
2025-09-06 19:00:13 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { useState } from "react";
 
 
-function Square(){
-  const [value , setValue] = useState(null);
+function Square({value}){
+  // const [value , setValue] = useState(null);
 
 
-  function handleClick(){
-    console.log("clicked");
-    setValue("X");
+  // function handleClick(){
+  //   console.log("clicked");
+  //   setValue("X");
     
-  }
+  // }
   return(
-    <button className="square" onClick={handleClick}>{value}</button>
+    <button className="square" >{value}</button>
   )
 }
 
