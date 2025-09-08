ef0cba7ae3a2854431336d363a662cf4c94520bc
Sujeet17m <sujeet17das@gmail.com>
2025-09-08 22:23:55 +0530

    updated props

--- Sujeet/src/App.jsx
+++ Sujeet/src/App.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import Card from './Card';
 
 function App() {
+  const data = [
+    { name: "Sujeet", profession: "Developer", image: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    { name: "Rahul", profession: "Designer", image: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { name: "Ankit", profession: "Manager", image: "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { name: "Rohit", profession: "Tester" , image: "https://images.unsplash.com/photo-1570158268183-d296b2892211?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+  ]
 
 
   return (
     <>
-    <Card text="Know More" color="bg-blue-600" />
-    <Card text="Submit" color="bg-red-600" />
+     <div className='w-full h-screen bg-slate-200 flex items-center justify-center gap-5'>
+      {data.map((item, index) => (
+        <Card key={index} image={item.image} name={item.name} profession={item.profession}/>
+      ))}
+     </div>
     </>
     
   );
--- Sujeet/src/Card.jsx
+++ Sujeet/src/Card.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import { FaArrowRight } from "react-icons/fa";
 
-function Card({ text,color }) {
+function Card({image, name, profession}) {
 
 
   return (
-    <button className={`px-3 py-1 ${color} text-zinc-100 text-xs rounded-md `}>{text}</button>
+    <div className='w-52 bg-white rounded-md overflow-hidden'>
+      <div className="w-full h-32 bg-sky-200" ></div>
+      <img className="w-20 h-20 rounded-full border-4 border-white -mt-10 ml-3" src={image} alt="user" />
+      <div className="w-full p-3"></div>
+        <h3 className="ml-4 mb-2 text-xl font-semibold" >{name}</h3>
+        <h5 className="ml-4 text-xs" >{profession}</h5>
+        <button className="mt-4 mb-2 ml-2 px-3 py-1 text-xs text-white bg-blue-500 font-semibold rounded-md">Add Friend</button>
+    </div>
   );
 }
 
